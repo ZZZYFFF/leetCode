d7bbe1880d48a68e6873dce6a537d3a5569d9070
zzzyfff <963991577@qq.com>
2021-04-05 23:35:58 +0800

    修复两数之和求值错误的bug

--- easy/src/com/leetCode/easy/TwoSum.java
+++ easy/src/com/leetCode/easy/TwoSum.java
@@ -7,16 +7,16 @@ import java.util.Map;
 
 /**
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
- *
+ * <p>
  * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
- *
+ * <p>
  * 示例:
- *
+ * <p>
  * 给定 nums = [2, 7, 11, 15], target = 9
- *
+ * <p>
  * 因为 nums[0] + nums[1] = 2 + 7 = 9
  * 所以返回 [0, 1]
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/two-sum
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -36,11 +36,11 @@ public class TwoSum {
 //        throw new  IllegalArgumentException("No two sum solution");
 //    }
 
-    public static int[] twoSum(int[] nums, int target){
+    public static int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[j] == target - nums[i]) {
-                    return new int[] { i, j };
+                    return new int[]{i, j};
                 }
             }
         }
@@ -49,17 +49,16 @@ public class TwoSum {
 
     /**
      * 两遍hash表
-     *
      */
-    public static int[] twoSum2(int[] nums,int target){
-        Map<Integer,Integer> map = new HashMap();
-        for (int i = 0;i < nums.length;i++){
-            map.put(nums[i],i);
+    public static int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
         }
-        for (int i = 0;i < nums.length;i++){
+        for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
-            if (map.containsKey(complement) && complement != nums[i]) {
-                return new int[]{i,map.get(complement)};
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[]{i, map.get(complement)};
             }
         }
         throw new IllegalArgumentException("No two sum solution");
@@ -68,29 +67,29 @@ public class TwoSum {
     /**
      * 一遍hash表
      */
-    public static int[] twoSum3(int[] nums,int target){
-        Map<Integer,Integer> map  =  new HashMap();
-        for (int i = 0;i<nums.length;i++){
+    public static int[] twoSum3(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
-            if (map.containsKey(complement) && complement != nums[i]) {
-                return new int[]{i,map.get(complement)};
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[]{i, map.get(complement)};
             }
-            map.put(nums[i],i);
+            map.put(nums[i], i);
         }
         throw new IllegalArgumentException("No two sum solution");
     }
 
     public static void main(String[] args) {
-        int[] nums = {2,4,5,6,78,8};
+        int[] nums = {2, 4, 5, 6, 78, 8};
         int target = 11;
-        int[] result = twoSum(nums,target);
-        int[] result2 = twoSum2(nums,target);
-        int[] result3 = twoSum3(nums,target);
+        int[] result = twoSum(nums, target);
+        int[] result2 = twoSum2(nums, target);
+        int[] result3 = twoSum3(nums, target);
 
 
-        System.out.println("result: "+Arrays.toString(result));
-        System.out.println("result: "+Arrays.toString(result2));
-        System.out.println("result: "+Arrays.toString(result3));
+        System.out.println("result: " + Arrays.toString(result));
+        System.out.println("result: " + Arrays.toString(result2));
+        System.out.println("result: " + Arrays.toString(result3));
 
     }
 }
